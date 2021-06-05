38c15a0d30e7977a137003824be4ba3653254a59
nobandegan <76049422+nobandegan@users.noreply.github.com>
2021-06-05 18:21:40 +0430

    test http

--- Source/Ino_OSS/Settings/GI_Ino.cpp
+++ Source/Ino_OSS/Settings/GI_Ino.cpp
@@ -5,8 +5,43 @@
 
 //IOnlineSubsystem* ion = IOnlineSubsystem::Get(FName("Ino_OSS")); for forcing to get the ino_oss
 
+void UGI_Ino::MyHttpCall()
+{
+	UE_LOG(LogTemp, Display, TEXT("MyHyypCall is called"));
+	TSharedRef<IHttpRequest, ESPMode::ThreadSafe> Request = Http->CreateRequest();
+	Request->OnProcessRequestComplete().BindUObject(this, &UGI_Ino::OnResponseReceived);
+	//This is the url on which to process the request
+	Request->SetURL("https://api.inoland.site/test/test.php");
+	Request->SetVerb("GET");
+	Request->SetHeader(TEXT("User-Agent"), "X-UnrealEngine-Agent");
+	Request->SetHeader("Content-Type", TEXT("application/json"));
+	Request->ProcessRequest();
+}
+
+void UGI_Ino::OnResponseReceived(FHttpRequestPtr Request, FHttpResponsePtr Response, bool bWasSuccessful)
+{
+	UE_LOG(LogTemp, Display, TEXT("OnResponseReceived is called"));
+	//Create a pointer to hold the json serialized data
+	TSharedPtr<FJsonObject> JsonObject;
+	//Create a reader pointer to read the json data
+	TSharedRef<TJsonReader<TCHAR>> Reader = TJsonReaderFactory<FString>::Create(Response->GetContentAsString());
+
+	//Deserialize the json data given Reader and the actual object to deserialize
+	if (FJsonSerializer::Deserialize(Reader, JsonObject))
+	{
+		//Get the value of the json object by field name
+		int32 recievedInt = JsonObject->GetIntegerField("customInt");
+		//Output it to the engine
+		GEngine->AddOnScreenDebugMessage(1, 2.0f, FColor::Green, FString::FromInt(recievedInt));
+	}
+}
+
 void UGI_Ino::Init()
 {
-	ReceiveInit();
-	UE_LOG(LogTemp, Error, TEXT("gi is initialized"));
+	//ReceiveInit();
+	UE_LOG(LogTemp, Display, TEXT("gi is initialized"));
+
+	Http = &FHttpModule::Get();
+	//MyHttpCall();
 }
+
--- Source/Ino_OSS/Settings/GI_Ino.h
+++ Source/Ino_OSS/Settings/GI_Ino.h
@@ -4,6 +4,10 @@
 
 #include "CoreMinimal.h"
 #include "Engine/GameInstance.h"
+
+#include "Http.h"
+
+
 #include "GI_Ino.generated.h"
 
 /**
@@ -13,6 +17,18 @@ UCLASS()
 class INO_OSS_API UGI_Ino : public UGameInstance
 {
 	GENERATED_BODY()
+
+public:
+	
+	FHttpModule* Http;
+
+	/* The actual HTTP call */
+	UFUNCTION(BlueprintCallable)
+	void MyHttpCall();
+
+	//UFUNCTION()
+	/*Assign this function to call when the GET request processes sucessfully*/
+	void OnResponseReceived(FHttpRequestPtr Request, FHttpResponsePtr Response, bool bWasSuccessful);
 
 protected:
 	virtual void Init() override;
